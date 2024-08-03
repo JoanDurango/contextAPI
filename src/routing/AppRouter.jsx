a7f8170699ce84b5f280520e1b407aac98e7bcb1
sebasdurango <sebasdurango19@gmail.com>
2024-08-02 19:39:54 -0500

    Inicio ContextApi, styles and some components

---
+++ src/routing/AppRouter.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "../components/Home";
+import { Articles } from "../components/Articles";
+
+export const AppRouter = () => {
+  return (
+    <Router>
+      {/* Menú de navegación*/}
+
+      {/*Configurar rutas*/}
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/inicio' element={<Home />}></Route>
+        <Route path='/articulos' element={<Articles />}></Route>
+        <Route path='*' element={
+            (
+                <div>
+                    <h1>Esta es la pagina de error</h1>
+                </div>
+            )
+        }></Route>
+      </Routes>
+    </Router>
+  )
+}
+
