f98e6381cb2d63090a879e495ac55b867992a9c5
sebasdurango <sebasdurango19@gmail.com>
2024-08-07 12:54:17 -0500

    Form validations

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,31 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Context } from "../context/Context";
 
 export const Login = () => {
 
   //Extraemos user y setUser del contexto usando useContext
-  const { user, setUser } = useContext(Context);
+  const { setUser } = useContext(Context);
+  const [username, setUsername] = useState('');
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
 
+  //Método para validar el campo username
+  const handleUsernameV = e => {
+    const value = e.target.value.replace(/\s+/g, '').toLowerCase();
+    setUsername(value);
+  };
+
+  //Método para validar el nombre y cambiar la primera letra a mayúscula
+  const handleNameV = e => {
+    const value = e.target.value.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ');
+    setName(value);
+  };
+
+  //Método para validar que se escriban solo números en el campo Teléfono
+  const handlePhoneV = e => {
+    const value = e.target.value.replace(/[^0-9]/g, '');
+    setPhone(value);
+  }
 
   //Metodo que se ejecuta al hacer click en el formulario
   const saveData = e => {
@@ -22,16 +42,20 @@ export const Login = () => {
     setUser(identific_user);
   };
 
+  /*   */
+
   return (
     <div>
       <h1 className="text-center">Login</h1>
       <form className="login" onSubmit={saveData}>
-        <input type="text" name="userName" placeholder="userName"/>
-        <input type="text" name="name" placeholder="Name"/>
-        <input type="text" name="phone" placeholder="Phone"/>
+        <input type="text" name="userName" placeholder="userName" value={username} onChange={handleUsernameV} required/>
+        <input type="text" name="name" placeholder="Name" value={name} onChange={handleNameV} required/>
+        <input type="text" name="phone" placeholder="Phone" value={phone} onChange={handlePhoneV} required/>
         <input type="submit" value="Enviar"/>
       </form>
     </div>
   )
 }
+
+
 
